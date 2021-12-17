3a77e95ba3d1cd7099090705c4c6e8a1e41943c8
MaNoller <96265697+MaNoller@users.noreply.github.com>
2021-12-17 05:40:46 +0100

    Update 4_HashTables.py

--- 4_HashTables.py
+++ 4_HashTables.py
@@ -54,3 +54,38 @@ max(arr[0:10])
 '''
 
 
+#PART 2
+print('PART 2:')
+#What was the temperature on Jan 9?
+
+weather = {}
+with open("nyc_weather.csv","r") as f:
+    f_header= next(f)
+    for line in f:
+        tokens = line.split(',')
+        day = tokens[0]
+        temp = float(tokens[1])
+        weather[day]=temp
+print(weather)
+
+print(weather['Jan 9'])
+
+#What was the temperature on Jan 4?
+print(weather['Jan 4'])
+
+
+#PART 3
+'''
+poem.txt Contains famous poem "Road not taken" by poet Robert Frost. You have to read this file in 
+python and print every word and its count as show below. Think about the best data structure that you 
+can use to solve this problem and figure out why you selected that specific data structure.
+'''
+
+with open("poem.txt","r") as f:
+    #lines = f.read().replace('\n',' ')
+    lines = f.read().split(' ')
+
+for line in lines:
+    lines = line.replace('\n', ' ')
+
+    print(line)
